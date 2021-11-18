10a06465f6a1ee13ee9961b832013bc49633eaf1
Thomas Decrick <thomas.decrick@colruytgroup.com>
2021-11-18 14:13:27 +0100

    clean up tests

--- Tests/GildedRoseTests/GildedRoseTests.swift
+++ Tests/GildedRoseTests/GildedRoseTests.swift
@@ -4,135 +4,126 @@ import XCTest
 class GildedRoseTests: XCTestCase {
 
     let app = GildedRose(items: [
-        Item(name: "Test item 0", sellIn: 5, quality: 50),
-        Item(name: "Test item 1", sellIn: 10, quality: 20),
-        // Item(name: "testQualityNeverMoreThanFifty", sellIn: 5, quality: 1000),
+        Item(name: "+5 Dexterity Vest", sellIn: 10, quality: 20),
+        Item(name: "Aged Brie", sellIn: 2, quality: 0),
+        Item(name: "Elixir of the Mongoose", sellIn: 5, quality: 7),
+        Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80),
+        Item(name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80),
+        Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 20),
+        Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 49),
+        Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 49),
+        Item(name: "Conjured Mana Cake", sellIn: 3, quality: 6)
     ])
 
     func testDemoteSellIn() {
-        // when
         app.updateQuality()
-
-        // then
-        XCTAssertEqual(app.items[0].sellIn, 4)
-        XCTAssertEqual(app.items[1].sellIn, 9)
+        XCTAssertEqual(app.items[0].sellIn, 9) // +5 Dexterity Vest
+        XCTAssertEqual(app.items[1].sellIn, 1) // Aged Brie
     }
 
-    func testDemoteQuality() {
-        // when
+    func testQuality() {
+        let app = GildedRose(items: [
+            Item(name: "Regular Item", sellIn: 5, quality: 5)
+        ])
         app.updateQuality()
-
-        // then
-        XCTAssertEqual(app.items[0].quality, 49)
-        XCTAssertEqual(app.items[1].quality, 19)
+        XCTAssertEqual(app.items[0].quality, 4)
     }
 
     func testQualityAfterSellDatePassed() {
-        // Once the sell by date has passed,
-        // Quality degrades twice as fast
-        app.items[0].sellIn = 0
-
-        // when
+        /* quality degrades twice as fast */
+        let app = GildedRose(items: [
+            Item(name: "Item", sellIn: 0, quality: 20)
+        ])
         app.updateQuality()
-
-        // then
-        XCTAssertEqual(app.items[0].quality, 48)
+        XCTAssertEqual(app.items[0].quality, 18)
     }
 
     func testQualityNeverNegative() {
-        // when
         for _ in 1...200 {
             app.updateQuality()
         }
-
-        // then
         for i in 0 ..< app.items.count {
             XCTAssertGreaterThanOrEqual(app.items[i].quality, 0)
         }
     }
 
     func testAgedBrieQualityIncrease() {
-        // given
-        let startQuality = 20
-        app.items.append(
-            Item(name: "Aged Brie", sellIn: 5, quality: startQuality)
-        )
-
-        // when
+        let initialQuality = app.items[1].quality
         app.updateQuality()
-
-        // then
-        XCTAssertGreaterThan(app.items.last!.quality, startQuality)
+        XCTAssertGreaterThan(app.items[1].quality, initialQuality)
+    }
+    
+    func testAgedBrieDoubleQualityIncrease() {
+        let app = GildedRose(items: [
+            Item(name: "Aged Brie", sellIn: 0, quality: 2)
+        ])
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].sellIn, -1)
+        XCTAssertEqual(app.items[0].quality, 4)
     }
 
     func testQualityNeverMoreThanFifty() {
-        // when 200 days pass
+        /* add quality 1000 to check if test fails */
+//        app.items.append(
+//            Item(name: "Item Quality over Fifty", sellIn: 5, quality: 1000)
+//        )
         for _ in 1...200 {
             app.updateQuality()
-        }
-
-        // then
-        for i in 0 ..< app.items.count {
-            XCTAssertLessThanOrEqual(app.items[i].quality, 50)
+            
+            for i in 0 ..< app.items.count {
+                if app.items[i].name != "Sulfuras, Hand of Ragnaros" {
+                    XCTAssertLessThanOrEqual(app.items[i].quality, 50)
+                }
+            }
         }
     }
 
     func testLegendarySulfuras() {
-        // given
-        app.items.append(
+        let app = GildedRose(items: [
             Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 20, quality: 80)
-        )
-        let startSellIn = app.items.last!.sellIn
-        let startQuality = app.items.last!.quality
-
-        // when 200 days pass
-        for _ in 1...200 {
-            app.updateQuality()
-        }
-
-        // then
-        XCTAssertEqual(app.items.last!.quality, startQuality)
-        XCTAssertEqual(app.items.last!.sellIn, startSellIn)
+        ])
+        app.updateQuality()
+        app.items[0].assertEqual(to: Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 20, quality: 80))
     }
 
     func testBackstagePassesQualityIncrease() {
-        // given
-        app.items.append(
-            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 11, quality: 10)
-        )
-        // when
+        let app = GildedRose(items: [
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 20, quality: 20)
+        ])
         app.updateQuality()
-        // then sellIn not less than 11, quality increases by 1
-        XCTAssertEqual(app.items.last!.quality, 11)
-
-        // when
+        XCTAssertEqual(app.items[0].sellIn, 19)
+        XCTAssertEqual(app.items[0].quality, 21)
+        
+        // sellIn not less than 11, quality increases by 1
+        app.items[0].sellIn = 11
         app.updateQuality()
-        // then sellIn is less than 11, quality increases by 2
-        XCTAssertEqual(app.items.last!.quality, 13)
-
-        // given
-        app.items.last!.sellIn = 5
-
-        // when
+        XCTAssertEqual(app.items[0].quality, 22)
+        
+        // sellIn is less than 11, quality increases by 2
+        app.items[0].sellIn = 10
         app.updateQuality()
-
-        // then sellIn is less than 6, quality increases by 3
-        XCTAssertEqual(app.items.last!.quality, 16)
+        XCTAssertEqual(app.items[0].quality, 24)
+        
+        // sellIn is less than 6, quality increases by 3
+        app.items[0].sellIn = 5
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].quality, 27)
     }
 
-    func testBackstagePassesQualityZero() {
-        // Backstage Passes Quality drops to 0 after the concert
-
-        // given
-        app.items.append(
+    func testBackstagePassesQualityZeroAfterConcert() {
+        let app = GildedRose(items: [
             Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 20)
-        )
-
-        // when
+        ])
         app.updateQuality()
-
-        // sellIn = 0, concert is over, quality should be 0
-        XCTAssertEqual(app.items.last!.quality, 0)
+        XCTAssertEqual(app.items[0].quality, 0)
     }
 
+}
+
+extension Item {
+    func assertEqual(to item: Item) {
+        XCTAssertEqual(self.name, item.name)
+        XCTAssertEqual(self.sellIn, item.sellIn)
+        XCTAssertEqual(self.quality, item.quality)
+    }
 }
