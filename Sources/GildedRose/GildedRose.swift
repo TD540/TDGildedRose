27070abac7cc30dcf484758c38575496119f31ed
Thomas Decrick <thomas.decrick@icloud.com>
2021-11-19 01:25:36 +0100

    rebalancing update method complexity

--- Sources/GildedRose/GildedRose.swift
+++ Sources/GildedRose/GildedRose.swift
@@ -1,76 +1,71 @@
 public class GildedRose {
     var items: [Item]
-    
-    public init(items: [Item]) {
-        self.items = items
-    }
-    
-    public func updateQuality() {
-        for item in items {
-            updateItemQuality(item: item)
-        }
-    }
 
     let agedBrie = "Aged Brie"
     let backstagePasses = "Backstage passes to a TAFKAL80ETC concert"
     let sulfuras = "Sulfuras, Hand of Ragnaros"
     let conjured = "Conjured Mana Cake"
+    
+    public init(items: [Item]) {
+        self.items = items
+    }
+    public func updateQuality() {
+        for item in items {
+            update(item)
+        }
+    }
 
-    private func updateItemQuality(item: Item) {
-        let degradeAmount = (item.name == conjured) ? 2 : 1
+    private func update(_ item: Item) {
         let itemDegrades = item.name != agedBrie && item.name != backstagePasses && item.name != sulfuras
+        let itemExpired = item.sellIn < 1
+
+        var degradeAmount: Int {
+            let amount = (item.name == conjured) ? 2 : 1
+            return itemExpired ? amount * 2 : amount
+        }
 
         if (itemDegrades) {
-            item.degradeQuality(by: degradeAmount)
+            degradeQuality(of: item, by: degradeAmount)
         }
-
         if (item.name == agedBrie) {
-            item.upgradeQuality()
+            upgradeQuality(of: item)
         }
-
         if (item.name == backstagePasses) {
-            item.upgradeQuality()
+            upgradeQuality(of: item)
 
             if (item.sellIn < 11) {
-                item.upgradeQuality()
+                upgradeQuality(of: item)
             }
             if (item.sellIn < 6) {
-                item.upgradeQuality()
+                upgradeQuality(of: item)
             }
         }
 
-        // all items except Sulfuras
         if (item.name != sulfuras) {
-            // decrease sellIn!
             item.sellIn = item.sellIn - 1
         }
 
-        // all items where sellIn date passed
-        if (item.sellIn < 0) {
-            if (itemDegrades) {
-                item.degradeQuality(by: degradeAmount)
-            }
+        if (itemExpired) {
             if (item.name == agedBrie) {
-                item.upgradeQuality()
+                upgradeQuality(of: item)
             } else if (item.name == backstagePasses) {
                 // Quality drops to 0 after the concert
                 item.quality = item.quality - item.quality
             }
         }
     }
-}
 
-extension Item {
-    func degradeQuality(by amount: Int = 1) {
-        let degradedQuality = quality - amount
+    private func degradeQuality(of item: Item, by amount: Int = 1) {
+        let degradedQuality = item.quality - amount
         if (0...50 ~= degradedQuality) {
-            quality = degradedQuality
+            item.quality = degradedQuality
         }
     }
-    func upgradeQuality(by amount: Int = 1) {
-        let upgradedQuality = quality + amount
+
+    private func upgradeQuality(of item: Item, by amount: Int = 1) {
+        let upgradedQuality = item.quality + amount
         if (0...50 ~= upgradedQuality) {
-            quality = upgradedQuality
+            item.quality = upgradedQuality
         }
     }
 }
